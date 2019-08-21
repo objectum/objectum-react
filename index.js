b9e59e11d5429b05187afce35c8e3b029f33cbf2
Dmitriy Samortsev <samortsev@gmail.com>
2019-08-21 15:55:06 +0300

    objectum-react

---
+++ index.js
@@ -0,0 +1,67 @@
+import Action from "./components/Action";
+import Auth from "./components/Auth";
+import BooleanField from "./components/BooleanField";
+import ChooseField from "./components/ChooseField";
+import Class from "./components/Class";
+import ClassAttr from "./components/ClassAttr";
+import ClassAttrs from "./components/ClassAttrs";
+import Classes from "./components/Classes";
+import Confirm from "./components/Confirm";
+import DateField from "./components/DateField";
+import Field from "./components/Field";
+import FileField from "./components/FileField";
+import Form from "./components/Form";
+import Grid from "./components/Grid";
+import Menu from "./components/Menu";
+import Menus from "./components/Menus";
+import NumberField from "./components/NumberField";
+import ObjectumApp from "./components/ObjectumApp";
+import Role from "./components/Role";
+import Roles from "./components/Roles";
+import SelectField from "./components/SelectField";
+import StringField from "./components/StringField";
+import Tab from "./components/Tab";
+import Tabs from "./components/Tabs";
+import TreeGrid from "./components/TreeGrid";
+import Types from "./components/Types";
+import User from "./components/User";
+import Users from "./components/Users";
+import View from "./components/View";
+import ViewAttr from "./components/ViewAttr";
+import ViewAttrs from "./components/ViewAttrs";
+import Views from "./components/Views";
+
+module.exports = {
+	Action,
+	Auth,
+	BooleanField,
+	ChooseField,
+	Class,
+	ClassAttr,
+	ClassAttrs,
+	Classes,
+	Confirm,
+	DateField,
+	Field,
+	FileField,
+	Form,
+	Grid,
+	Menu,
+	Menus,
+	NumberField,
+	ObjectumApp,
+	Role,
+	Roles,
+	SelectField,
+	StringField,
+	Tab,
+	Tabs,
+	TreeGrid,
+	Types,
+	User,
+	Users,
+	View,
+	ViewAttr,
+	ViewAttrs,
+	Views
+};
