8605607408fd42ba03cfdf0805f9364a927246e4
Dmitriy Samortsev <samortsev@gmail.com>
2020-06-24 12:07:30 +0300

    objectum-react

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,15 +3,9 @@ import React, {Component} from "react";
 class Pagination extends Component {
 	constructor (props) {
 		super (props);
-		
-		this.state = {
-			active: this.props.active
-		};
 	}
 	
 	onClick (active) {
-		this.setState ({active});
-		
 		if (this.props.onChange) {
 			this.props.onChange (active);
 		}
@@ -21,13 +15,13 @@ class Pagination extends Component {
 		let me = this;
 		
 		if (!me.props.items) {
-			return <div>no items</div>;
+			return null;
 		}
 		return (
 			<ul className={me.props.className || "pagination"}>
 				{this.props.items.map ((item, i) => {
 					return (
-						<li key={i} className={`page-item ${(i === me.props.active || item === me.props.active) ? "active" : ""}`}>
+						<li key={i} className={`page-item ${(i === me.props.active) ? "active" : ""}`}>
 							<button className="page-link" onClick={() => me.onClick (i)}>{item}</button>
 						</li>
 					);
