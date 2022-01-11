aceeabaf181bf711b40bce41b9e5484a2f1c93a0
Dmitry Samortsev <samortsev@gmail.com>
2022-01-11 22:26:19 +0300

    objectum-react

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -10,17 +10,19 @@ export default class Loading extends Component {
 	}
 	
 	render () {
+		let label = this.props.hasOwnProperty ("label") ? this.props.label : (i18n ("Loading") + " ...");
+
 		if (this.props.container) {
 			return <div className="container">
 				<div className="mt-2 p-4 border shadow-sm d-flex bg-white">
 					<div className="spinner-border spinner-border-sm mr-2 my-auto" role="status" aria-hidden="true" />
-					<div className="my-auto">{i18n ("Loading") + " ..."}</div>
+					<div className="my-auto">{label}</div>
 				</div>
 			</div>;
 		}
 		return <div className="d-flex">
 			<div className="spinner-border spinner-border-sm mr-2 my-auto" role="status" aria-hidden="true" />
-			<div className="my-auto">{i18n ("Loading") + " ..."}</div>
+			<div className="my-auto">{label}</div>
 		</div>;
 	}
 };
